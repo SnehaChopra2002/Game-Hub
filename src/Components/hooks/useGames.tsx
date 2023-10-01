d880d09b2b4b50cbe63d77e6f3e1adbaec6435bd
SnehaChopra2002 <sneha.chopra46@gmail.com>
2023-10-01 14:03:23 +0530

    final commit

--- src/Components/hooks/useGames.tsx
+++ src/Components/hooks/useGames.tsx
@@ -64,7 +64,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
-  rating_top:number
+  rating_top:number;
 }
 
 const useGames = (gameQuery:GameQuery) =>
